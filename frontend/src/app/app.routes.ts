be3d8ab467957e8f161f89650ba9d8e8affe4ede
Ostap <ostaplozovyy25@gmail.com>
2025-11-30 13:05:54 +0100

    feat: adding priests to parish

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ParishListComponent } from './components/parish-list/parish-list.component';
 import { ParishFormComponent } from './components/parish-form/parish-form.component';
 import { ParishDetailsComponent } from './components/parish-details/parish-details.component';
+import { PriestFormComponent } from './components/priest-form/priest-form.component';
 
 export const routes: Routes = [
     {path: 'parishes', component: ParishListComponent},
     {path: 'parishes/new', component: ParishFormComponent},
     {path: 'parishes/:id/edit', component: ParishFormComponent},
-    {path: 'parishes/:id', component: ParishDetailsComponent}
+    {path: 'parishes/:id', component: ParishDetailsComponent},
+    {path: 'parishes/:parishId/priests/new', component: PriestFormComponent}
 ];
