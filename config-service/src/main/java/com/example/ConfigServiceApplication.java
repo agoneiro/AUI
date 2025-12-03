d5ab915b5d5c838f4a5e1b10ba4dd3e1be0cc6d2
Ostap <ostaplozovyy25@gmail.com>
2025-12-03 12:44:46 +0100

    fix: priests can be added to parish

--- config-service/src/main/java/com/example/ConfigServiceApplication.java
+++ config-service/src/main/java/com/example/ConfigServiceApplication.java
@@ -7,10 +7,7 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
 public class ConfigServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServiceApplication.class, args);
