a7eae3895331e73822087a43d1f567201c7306e6
Ostap <ostaplozovyy25@gmail.com>
2025-12-03 12:27:38 +0100

    working :D

--- config-service/src/main/java/com/example/ConfigServiceApplication.java
+++ config-service/src/main/java/com/example/ConfigServiceApplication.java
@@ -2,10 +2,15 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public class ConfigServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServiceApplication.class, args);
